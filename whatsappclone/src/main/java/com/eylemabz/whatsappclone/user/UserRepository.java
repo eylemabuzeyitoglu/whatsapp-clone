79ec3b69966381361d17c456dfcd90e15b77db69
EylemAbz <140246327+EylemAbz@users.noreply.github.com>
2025-03-21 21:18:40 +0300

    spring security

---
+++ whatsappclone/src/main/java/com/eylemabz/whatsappclone/user/UserRepository.java
@@ -0,0 +1,15 @@
+package com.eylemabz.whatsappclone.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,String > {
+
+    @Query(name = UserConstants.FIND_USER_BY_EMAIL)
+    Optional<User> findByEmail(@Param("email") String userEmail);
+}
