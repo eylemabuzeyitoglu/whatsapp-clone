d6999a6c61f01e66b5afb44d07da4d4b8ad906e5
EylemAbz <140246327+EylemAbz@users.noreply.github.com>
2025-03-25 11:31:41 +0300

    Implement the User domain

--- whatsappclone/src/main/java/com/eylemabz/whatsappclone/user/UserRepository.java
+++ whatsappclone/src/main/java/com/eylemabz/whatsappclone/user/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +16,7 @@ public interface UserRepository extends JpaRepository<User,String > {
 
     @Query(name = UserConstants.FIND_USERS_BY_PUBLIC_ID)
     Optional<User> findByPublicId(String publicId);
+
+    @Query(UserConstants.FIND_ALL_USERS_EXCEPT_SELF)
+    List<User> findAllUsersExceptSelf(@Param("publicId") String senderId);
 }
